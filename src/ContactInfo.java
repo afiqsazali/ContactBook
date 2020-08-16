eeac65340fdce96514af957da4d7f75b31fb523f
Afiq Sazali <afiqsazali@gmail.com>
2020-08-16 21:19:14 +0800

    ommit 3

--- src/ContactInfo.java
+++ src/ContactInfo.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class ContactInfo {
 
 		String name;
-		long number;
+		String number;
 		String address;
 
-		public ContactInfo(String name, int number, String address) {
+		public ContactInfo() {
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("Name : ");
+				String name = scanner.nextLine();
+				System.out.println("Phone number : ");
+				String number = scanner.nextLine();
+				System.out.println("Address : ");
+				String address = scanner.nextLine();
+
 				this.name = name;
 				this.number = number;
 				this.address = address;
@@ -18,11 +28,11 @@ public String getName() {
 				this.name = name;
 		}
 
-		public long getNumber() {
+		public String getNumber() {
 				return number;
 		}
 
-		public void setNumber(int number) {
+		public void setNumber(String number) {
 				this.number = number;
 		}
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        ContactInfo person1 = new ContactInfo("afiq",12123,"noice 12312312 wdawdawd");
-        ContactInfo person2 = new ContactInfo("faris",121232,"noice 12312312 wdawd1231awd");
+        ContactInfo person1 = new ContactInfo();
+        ContactInfo person2 = new ContactInfo();
 
         System.out.println(person1.name);
         System.out.println(person2.name);
