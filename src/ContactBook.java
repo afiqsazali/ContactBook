f4e69cb79f1cb4b0d3d091be17517f44183169ab
fakhrihensem <fakhrilokman1998@gmail.com>
2020-08-17 18:32:56 +0800

    best nuke code change my mind

--- src/ContactBook.java
+++ src/ContactBook.java
@@ -2,55 +2,55 @@ import java.util.Scanner;
 
 public class ContactBook {
 
-		LinkedList contactList = new LinkedList();
+	LinkedList contactList = new LinkedList();
 
-		Scanner input = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
 
-		public void add() {
-			Contact person = new Contact();
-			System.out.print("Enter name: ");
-			String Name = input.nextLine();
-			person.setName(Name);
+	public void add() {
+		Contact person = new Contact();
+		System.out.print("Enter name: ");
+		String Name = input.nextLine();
+		person.setName(Name);
 
-			System.out.print("\nEnter email: ");
-			String email = input.nextLine();
-			person.setEmail(email);
+		System.out.print("\nEnter email: ");
+		String email = input.nextLine();
+		person.setEmail(email);
 
-			System.out.print("\nEnter phone number: ");
-			String phoneNum = input.nextLine();
-			person.setphoneNum(email);
+		System.out.print("\nEnter phone number: ");
+		String phoneNum = input.nextLine();
+		person.setphoneNum(email);
 
-			System.out.print("\nEnter Posmail: ");
-			String PosMail = input.nextLine();
-			person.setPosMail(PosMail);
+		System.out.print("\nEnter Posmail: ");
+		String PosMail = input.nextLine();
+		person.setPosMail(PosMail);
 
-			contactList.insert(person);
+		contactList.insert(person);
+	}
+
+	public void search() {
+
+		if (contactList.insert(person) == 0) {
+			System.out.println("No record inside contact list");
+			return;
 		}
 
-		public void search() {
-
-			if (contactList.insert(person) == 0) {
-				System.out.println("No record inside contact list");
-				return;
-			}
-
-			//			System.out.println("Name in contact list");
-			//			for (contactList look: this.contactList){
-			//				System.out.println(look.getName());
-			//				for (int i = look.getName().length();i<999;i++){
-			//					System.out.println(" ");
-			//				}
-			//			}
-			System.out.println("Enter a name for search contact");
-			String newName = input.next();
-			if (String name == newName){
-				System.out.println(contactList.insert(person));
-			}
-				else{
-				System.out.println("No name recorded in contact");
-			}
+		//			System.out.println("Name in contact list");
+		//			for (contactList look: this.contactList){
+		//				System.out.println(look.getName());
+		//				for (int i = look.getName().length();i<999;i++){
+		//					System.out.println(" ");
+		//				}
+		//			}
+		System.out.println("Enter a name for search contact");
+		String newName = input.next();
+		if (String name == newName()){
+			System.out.println(contactList.insert(person));
 		}
-
+		else if (String name != newName){
+			System.out.println("No name recorded in contact");
+		}
+	}
+}
 
 //
 //		public void edit(String name) {
@@ -61,4 +61,4 @@ public class ContactBook {
 //
 //		}
 
-}
+
