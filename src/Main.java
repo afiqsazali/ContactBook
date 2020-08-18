293b8d96ccdadfb74b0f0a523ffc73bad20814ca
ady <adyinthattic@gmail.com>
2020-08-18 10:53:32 +0800

    adadsa

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,11 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         while(true){
-            System.out.println("1 = add");
-            System.out.println("2= search");
-            System.out.println("3= edit");
-            System.out.println("4= remove");
-            System.out.println("5= exit");
+            System.out.println("Press '1' to add contact");
+            System.out.println("Press '2' to search contact");
+            System.out.println("Press '3' to edit contact");
+            System.out.println("Press '4' to remove contact");
+            System.out.println("Press '5' to exit");
             System.out.println("Test jorr");
             int choice = scan.nextInt();
 
