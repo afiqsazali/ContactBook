7ed80fc92f9c4d6654ef56037f178973a8dde3e1
fakhrihensem <fakhrilokman1998@gmail.com>
2020-08-18 10:37:42 +0800

    best nuke code change my mind

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         ContactBook contactList1 = new ContactBook();
+        Scanner scan = new Scanner(System.in);
 
-        contactList1.add();
-        contactList1.search();
-        contactList1.edit();
-        contactList1.remove();
+        while(true){
+            System.out.println("1 = add");
+            System.out.println("2= search");
+            System.out.println("Test jorr");
+            int choice = scan.nextInt();
 
+            switch(choice){
+                case 1: contactList1.add();
+                    break;
+                case 2: contactList1.search();
+                    break;
+                case 3: contactList1.edit();
+                    break;
+                case 4: contactList1.remove();
+                    break;
+            }
+        }
     }
-
 }
